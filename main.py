47f056597a4155ae4c7bfed4c1b83a739886269a
Петя Питаль <PITAL.PETYA@YANDEX.RU>
2024-06-16 17:26:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ import solve_hypocentr as sh
 import  writer
 import seismic_sensors as ss
 import  visual
+import test_calcs as tc
 
 filepath='Antonovskaya_joint_test.xlsx'
 filepath2='Antonovskaya_gauges.txt'
 filepath3='newAntonovskaya_joint_test.xlsx'
-sc.massive_calculation_from_file_path(filepath,filepath2,99,0.000001)
+tc.gss_massive_calculation_from_file_path(filepath,filepath2,4,200)
+# tc.geiger_massive_calculation_from_file_path(filepath,filepath2,99,0.000001)
 visual.plot_coordinates_from_excel(filepath3)

e2119ffb5ee156b0b836987f6ecfa334be9a4bf0
Петя Питаль <PITAL.PETYA@YANDEX.RU>
2024-07-02 00:56:34 +0300

    Update solve_hypocentr.py

--- solve_hypocentr.py
+++ solve_hypocentr.py
@@ -7,7 +7,7 @@ import seismic_sensors
 import points
 import pandas as pd
 import numpy as  np
-
+import jhd
 #import square_compute
 
 class Problem:
@@ -47,7 +47,7 @@ class EventDate:
         self.date = date
         self.real_coordinates = real_coordinates
         self.speed = speed
-        # self.number_of_sensors = number_of_sensors
+        self.number_of_sensors = number_of_sensors
         self.arrival_times = arrival_times
 
     def __repr__(self):
@@ -62,12 +62,12 @@ def  create_list_event_date_from_exel(filepath):
             date=row['Дата и время UTC+7'],
             real_coordinates=[row['X'], row['Y'], row['Z']],
             speed=row['V'],
-            # number_of_sensors=row['N датчиков'],
+            number_of_sensors=row['N датчиков'],
             arrival_times=row[['intro_1', 'intro_2', 'intro_3', 'intro_4', 'intro_5', 'intro_6', 'intro_7']].tolist()
         )
         for index, row in events_data.iterrows()
     ]
-    print(list_of_events)
+    # print(list_of_events)
     return list_of_events
 
 def create_location(filepath):
@@ -78,6 +78,7 @@ def create_location(filepath):
     data[:, 1:4] = np.char.replace(data[:, 1:4], ',', '.')
     # Массив с координатами
     coordinates = data[:, 1:4].astype(float)
+    return coordinates
 
 # Загрузка данных из Excel файла, пропуская первые 4 строки с метаданными
 def create_list_of_problem_from_file(filepath1,filepath2):
@@ -133,3 +134,70 @@ def find_hypocenter(x, y, z, t, v, initial_guess):
 # create_problem(led[0],l)
 #create_list_of_problem_from_file(filepath,filepath2)
 #sc.massive_calculation_from_file_path(filepath,filepath2,1000,0.1)
+#Создаем массив из времен
+# наблюдений
+
+def create_observed_times_list_and_num_events(events_date_list):
+    num_events=0
+    OT_list=[]
+    # edl = create_list_event_date_from_exel(filepath)
+    for ed in events_date_list:
+        OT=ed.arrival_times
+        # print(OT)
+        OT_list.append(OT)
+    # print(OT_list)
+    num_events=len(OT_list)
+    return OT_list, num_events
+
+def create_initial_estimates(num_events):
+    initial_estimates=[]
+    for i in range(num_events):
+        initial_estimates.append([0, 0, 0, 0])
+    # print(initial_estimates)
+    return initial_estimates
+class Station:
+    def __init__(self, x, y, z, name=None, correction=0):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.name = name
+        self.correction = correction
+    def __repr__(self):
+        return (f"Station(x={self.x}, y={self.y}, z={self.z} ,name={self.name},correction={self.correction})")
+
+def create_stations_and_num_stations(filepath):
+    l = create_location(filepath)
+    # print(l)
+    stations=[]
+    num_stations=len(l)
+    for i in range(num_stations):
+        station=Station(l[i,0],l[i,1],l[i,2])
+        # print(station)
+        stations.append(station)
+    # print(stations,num_stations)
+    return stations,num_stations
+def create_real_cords_from_event_date_list(event_date_list):
+    real_cords=[]
+    for ed in event_date_list:
+        real_cords.append(ed.real_coordinates)
+    #print(real_cords)
+    return real_cords
+
+
+edl=create_list_event_date_from_exel('Antonovskaya_joint_test.xlsx')
+real_cords=create_real_cords_from_event_date_list(edl)
+OTl, num_events=create_observed_times_list_and_num_events(edl)
+# print(OTl)
+# print(num_events)
+ie=create_initial_estimates(num_events)
+# print(ie)
+stations, num_stations=create_stations_and_num_stations('Antonovskaya_gauges.txt')
+equations=jhd.create_equations_type_3(OTl,ie,num_events,num_stations,stations)
+DeltaX,DeltaS=jhd.solve_joint_hypocenter(equations,num_events,num_stations)
+focal_param=jhd.get_focal_parameters(DeltaX,num_events)
+jhd.print_focal_parameters(focal_param)
+calc_cords=jhd.get_coordinates_without_time(focal_param)
+jhd.compare_real_and_computed(real_cords,calc_cords)
+jhd.plot_comparison_all(real_cords,calc_cords)
+
+
